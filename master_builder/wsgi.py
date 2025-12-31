f2175918f61bcd3dd405f9646279f8d966ea87f3
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-12-31 19:12:31 +0100

    Change Django settings module to production

--- master_builder/wsgi.py
+++ master_builder/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "master_builder.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "master_builder.settings.production")
 
 application = get_wsgi_application()
